51b7932b678f23a461bbe2cf176d093a2a265a18
ajorgen3 <113084414+ajorgen3@users.noreply.github.com>
2023-01-11 18:15:59 -0500

    nearly complete SQLite3 rewrite, 2 databases

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from datetime import datetime, date, timezone
+from datetime import datetime, timedelta, timezone
 import time
 import discord
 from discord.ext.commands import Bot
 from discord import Intents
 import statistics
 import matplotlib as MPL
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import matplotlib.dates as MPLDates
 import sqlite3
 import os
@@ -29,8 +29,8 @@ userStatsMap = {}
 # Maps the name of the ranking chart (string) to a sorted list of tuples. Each tuple contains the user being ranked (index 0)
 # and the data being ranked by (index 1). The data is sorted by index 1.
 rankingsMap = {}
-#database name
-DBName = "message_count_database.db"
+#database name. Set to test server by default
+DBName = "951979508649562162.db"
 #sqlite3 connection
 conn = sqlite3.connect(DBName, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
 
@@ -72,6 +72,18 @@ async def test2(context):
     for count in messageCountList:
         print(str(count) + "\n")
 
+@bot.command(name='test3')
+async def test3(context):
+    channel = context.message.channel
+    user = getUser(context)
+    global conn, DBName
+
+    await getServer(context)
+
+    cursor = conn.execute("SELECT DATE, MESSAGECOUNT FROM MessageCounts WHERE ID=?;", (user.id,))
+    for row in cursor:
+        print("date: ", row[0], " message count: ", row[1])
+
 @bot.command(name='DBTest')
 async def DBTest(context):
     global conn
@@ -105,7 +117,7 @@ async def DBTest(context):
     conn.commit()
     print("values added")
 
-    cursor = conn.execute("SELECT id, name, address, salary FROM COMPANY")
+    cursor = conn.execute("SELECT id, name, address, salary FROM COMPANY;")
     for row in cursor:
         print("ID = ", row[0])
         print("NAME = ", row[1])
@@ -121,8 +133,12 @@ async def DBTest(context):
 # Prints the total number of messages for the server in the channel the command was sent in.
 @bot.command(name='totalservermessages')
 async def totalServerMessages(context):
-    global totalMessages
-    await context.message.channel.send("Total server messages: " + str(totalMessages))
+    global totalMessages, conn
+
+    await getServer(context)
+    cursor = conn.execute("SELECT SUM(MESSAGECOUNT) FROM MessageCounts;")
+
+    await context.message.channel.send("Total server messages: " + str(cursor.fetchone()[0]))
 
 
 # Prints the total number of messages for the target in the channel the command was sent in.
@@ -132,7 +148,10 @@ async def totalUserMessages(context):
     message = context.message
     user = getUser(context)
 
-    await message.channel.send(user.mention + "'s total message count: " + str(userStatsMap[user]["total messages"]))
+    await getServer(context)
+    cursor = conn.execute("SELECT SUM(MESSAGECOUNT) FROM MessageCounts WHERE ID=?;", (user.id,))
+
+    await message.channel.send(user.display_name + "'s total message count: " + str(cursor.fetchone()[0]))
 
 
 # Prints the percent of the total messages in the server that were sent by the target in the channel the command was sent in.
@@ -142,31 +161,46 @@ async def percentTotal(context):
     global totalMessages
     user = getUser(context)
     channel = context.message.channel
-    percent = int(userStatsMap[user]["total messages"] / totalMessages * 10000) / 100
 
-    await channel.send(user.mention + " has sent " + str(percent) + "% of the server's total messages")
+    await getServer(context)
+    cursorT = conn.execute("SELECT SUM(MESSAGECOUNT) FROM MessageCounts;")
+    cursorU = conn.execute("SELECT SUM(MESSAGECOUNT) FROM MessageCounts WHERE ID=?;", (user.id,))
+
+    percent = int(cursorU.fetchone()[0] / cursorT.fetchone()[0] * 10000) / 100
+
+    await channel.send(user.display_name + " has sent " + str(percent) + "% of the server's total messages")
 
 
 # Prints a list of stats about the target in the channel the command was sent in.
 # The target is determined by getUser()
 # Stats: total messages sent, mean, median, mode, range, min, max, standard deviation, number of days active
-@bot.command(name='stats')
+@bot.command(name='stats') # fix stats eventually to add median, mode, and standard deviation (if possible)
 async def stats(context):
     message = context.message
     user = getUser(context)
+    global conn
 
-    await message.channel.send("Note: All stats are based exclusively on days the user sent messages.")
+    await getServer(context)
 
-    await message.channel.send(user.mention+ "'s statistics:"
-                                + "\nTotal message count: "+ str(userStatsMap[user]["total messages"])
-                                + "\nMean daily messages: " + str(userStatsMap[user]["mean"])
-                                + "\nMedian daily messages: "+ str(userStatsMap[user]["median"])
-                                + "\nMode daily messages: " + str(userStatsMap[user]["mode"])
-                                + "\nMax messages in 1 day: " + str(userStatsMap[user]["maximum"])
-                                + "\nMin messages in 1 day: " + str(userStatsMap[user]["minimum"])
-                                + "\nRange: " + str(userStatsMap[user]["minimum"]) + " - " + str(userStatsMap[user]["maximum"])
-                                + (("\nStandard Deviation: " + str(userStatsMap[user]["SD"])) if (userStatsMap[user]["days active"] >= 2) else "")
-                                + "\nNumber of days active: " + str(userStatsMap[user]["days active"]))
+    cursorTotal = conn.execute("SELECT SUM(MESSAGECOUNT) FROM MessageCounts WHERE ID=?;", (user.id,))
+    cursorMean = conn.execute("SELECT AVG(MESSAGECOUNT) FROM MessageCounts WHERE ID=?;", (user.id,))
+    cursorMax = conn.execute("SELECT MAX(MESSAGECOUNT) FROM MessageCounts WHERE ID=?", (user.id,))
+    cursorMin = conn.execute("SELECT MIN(MESSAGECOUNT) FROM MessageCounts WHERE ID=?", (user.id,))
+    cursorNumDays = conn.execute("SELECT COUNT(*) FROM MessageCounts WHERE ID = ?;", (user.id,))
+
+    total = cursorTotal.fetchone()[0]
+    mean = cursorMean.fetchone()[0]
+    max = cursorMax.fetchone()[0]
+    min = cursorMin.fetchone()[0]
+    numDays = cursorNumDays.fetchone()[0]
+
+    await message.channel.send(user.display_name + "'s statistics:"
+                                + "\nTotal message count: "+ str(total)
+                                + "\nMean daily messages: " + str(mean)
+                                + "\nMax messages in 1 day: " + str(max)
+                                + "\nMin messages in 1 day: " + str(min)
+                                + "\nRange: " + str(min) + " - " + str(max)
+                                + "\nNumber of days since joining: " + str(numDays))
 
 
 ########################
@@ -181,12 +215,19 @@ async def messageChart(context):
     channel = context.message.channel
     user = getUser(context)
     toPrint = "Top message chart:\n"
+    place = 1
+    global conn
 
-    for idx, pair in enumerate(rankingsMap["most message chart"]):
-        if (pair[0] == user):
-            toPrint += "**" + str(idx + 1) + ") " + pair[0].name + " - " + str(pair[1]) + "**\n"
+    await getServer(context)
+    cursor = conn.execute("SELECT ID, SUM(MESSAGECOUNT) FROM MessageCounts GROUP BY ID ORDER BY 2 DESC")
+
+    for row in cursor:
+        if (row[0] == user.id):
+            toPrint += "**" + str(place) + ") " + (await bot.fetch_user(row[0])).display_name + " - " + str(row[1]) + "**\n"
         else:
-            toPrint += str(idx + 1) + ") " + pair[0].name + " - " + str(pair[1]) + "\n"
+            toPrint += str(place) + ") " + (await bot.fetch_user(row[0])).display_name + " - " + str(row[1]) + "\n"
+        place += 1
+
 
     await channel.send(toPrint)
 
@@ -195,77 +236,116 @@ async def messageChart(context):
 ##### Graphs #####
 ##################
 
-@bot.command(name="messageovertimegraph")   # bugged, see general for thoughts yesterday. instead of making a line graph, try doing a scatterplot instead
+# Graphs a users total messages sent over time. Can print for 1 or multiple users.
+@bot.command(name="messageovertimegraph")
 async def messageovertimegraph(context):
+    channel = context.message.channel
+    users = getUserGraphs(context)
+    dateList = {}
+    totalMessages = {}
+    longestPeriod = 0
+    global conn
+
+    await getServer(context)
+
+    for user in users:
+        cursor = conn.execute("SELECT DATE, MESSAGECOUNT FROM MessageCounts WHERE ID=?", (user.id,))
+        i = 0
+        dateListT = []
+        totalMessagesT = []
+        for row in cursor:
+            dateListT.append(row[0])
+            if (i == 0):
+                totalMessagesT.append(row[1])
+            else:
+                totalMessagesT.append(row[1] + totalMessagesT[i - 1])
+            i += 1
+        dateList[user] = dateListT
+        totalMessages[user] = totalMessagesT
+        currentPeriod = len(dateListT)
+        if (currentPeriod > longestPeriod):
+            longestPeriod = currentPeriod
+
+    plt.gca().xaxis.set_major_formatter(MPLDates.DateFormatter('%m/%d/%Y'))
+    plt.gca().xaxis.set_major_locator(MPLDates.DayLocator(interval=int(longestPeriod / 4)))
+
+    for user in users:
+        plt.plot(dateList[user], totalMessages[user])
+
+    plt.gcf().autofmt_xdate()
+    plt.savefig("messageovertimegraph.png")
+
+    with open('messageovertimegraph.png', 'rb') as img:
+        await channel.send(file=discord.File(img))
+
+    plt.clf()
+    plt.cla()
+
+#
+@bot.command(name="dailymessagesgraph")
+async def dailymessagesgraph(context):
     channel = context.message.channel
     user = getUser(context)
     dateList = []
-    totalMessageDates = []
+    messageCounts = []
+    global conn
 
-    for dateT in list(messageCountDatabase[user].keys()):
-        dateList.append(dateT.date())
+    await getServer(context)
 
-    i = 0
-    for count in list(messageCountDatabase[user].values()):
-        if (i == 0):
-            totalMessageDates.append(count)
-        else:
-            totalMessageDates.append(count + totalMessageDates[i - 1])
-        i += 1
+    cursor = conn.execute("SELECT DATE, MESSAGECOUNT FROM MessageCounts WHERE ID=?", (user.id,))
+    for row in cursor:
+        dateList.append(row[0])
+        messageCounts.append(row[1])
 
-    plot.gca().xaxis.set_major_formatter(MPLDates.DateFormatter('%m/%d/%Y'))
-    plot.gca().xaxis.set_major_locator(MPLDates.DayLocator(interval=int(len(dateList)/4))) # use difference between first and last date instead of length. length only works for active people
-    plot.plot(dateList, totalMessageDates)
-    plot.gcf().autofmt_xdate()
-    plot.savefig("messageovertimegraph.png")
+    cursor = conn.execute("SELECT COUNT(*) FROM MessageCounts WHERE ID=?", (user.id,))
+    plt.gca().xaxis.set_major_formatter(MPLDates.DateFormatter('%m/%d/%Y'))
+    plt.gca().xaxis.set_major_locator(MPLDates.DayLocator(interval=int((cursor.fetchone())[0] / 4)))
+    plt.plot(dateList, messageCounts)
+    plt.gcf().autofmt_xdate()
+    plt.savefig("messageovertimegraph.png")
 
     with open('messageovertimegraph.png', 'rb') as img:
         await channel.send(file=discord.File(img))
 
-    plot.clf()
-    plot.cla()
+    plt.clf()
+    plt.cla()
 
 
-@bot.command(name='messageovertimegraphserver')
-async def messageovertimegraph(context):
+@bot.command(name="dailymessagesgraphserver")
+async def dailymessagesgraphserver(context):
     channel = context.message.channel
     dateList = {}
-    totalMessageDates = {}
-    longestLength = 0
+    messageCounts = {}
+    global conn
+
+    await getServer(context)
 
     for user in context.message.guild.members:
-        tempList = []
-        for dateT in list(messageCountDatabase[user].keys()):
-            tempList.append(dateT.date())
-        dateList[user] = tempList
+        cursor = conn.execute("SELECT DATE, MESSAGECOUNT FROM MessageCounts WHERE ID=?", (user.id,))
+        dateListT = []
+        messageCountsT = []
+        for row in cursor:
+            dateListT.append(row[0])
+            messageCountsT.append(row[1])
+        dateList[user] = dateListT
+        messageCounts[user] = messageCountsT
 
-        if (len(tempList) > longestLength):
-            longestLength = len(tempList)
+    cursorNumDays = conn.execute("SELECT COUNT(*) FROM MessageCounts GROUP BY ID ORDER BY COUNT(*) DESC LIMIT 1;")
 
-        i = 0
-        tempList = []
-        for count in list(messageCountDatabase[user].values()):
-            if (i == 0):
-                tempList.append(count)
-            else:
-                tempList.append(count + tempList[i - 1])
-            i += 1
-        totalMessageDates[user] = tempList
-
-    plot.gca().xaxis.set_major_formatter(MPLDates.DateFormatter('%m/%d/%Y'))
-    plot.gca().xaxis.set_major_locator(MPLDates.DayLocator(interval=int(longestLength / 4)))
+    plt.gca().xaxis.set_major_formatter(MPLDates.DateFormatter('%m/%d/%Y'))
+    plt.gca().xaxis.set_major_locator(MPLDates.DayLocator(interval=int((cursorNumDays.fetchone())[0] / 4)))
 
     for user in context.message.guild.members:
-        plot.plot(dateList[user], totalMessageDates[user])
+        plt.plot(dateList[user], messageCounts[user])
 
-    plot.gcf().autofmt_xdate()
-    plot.savefig("messageovertimegraph.png")
+    plt.gcf().autofmt_xdate()
+    plt.savefig("messageovertimegraph.png")
 
     with open('messageovertimegraph.png', 'rb') as img:
         await channel.send(file=discord.File(img))
 
-    plot.clf()
-    plot.cla()
+    plt.clf()
+    plt.cla()
 
 
 #####################################
@@ -304,24 +384,18 @@ async def update(context):  # remakes the database of message counts
         members = guild.members
 
         conn.close()
-        filepath = "C:\\Users\\mangu\\PycharmProjects\\Discord Bot\\" + DBName
+        DBName = str(guild.id) + ".db"
+        filepath = "C:\\Users\\mangu\\PycharmProjects\\DiscordBot\\" + DBName
         os.remove(filepath)
         conn = sqlite3.connect(DBName, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
         conn.execute('''CREATE TABLE IF NOT EXISTS MessageCounts
-                    (ID             INT PRIMARY KEY    NOT NULL,
-                    DATE            TIMESTAMP
+                    (ID             INT             NOT NULL,
+                    DATE            TIMESTAMP,
                     MESSAGECOUNT    INT DEFAULT 0);''')
         await messageOriginal.channel.send("Message count table created.")
 
         for user in members:
             messageCountDatabase[user] = {}
-            #insert cols into MessageCounts for each date
-            dateTemp = utc_to_local(guild.created_at)
-            dateTemp = datetime(dateTemp.year, dateTemp.month, dateTemp.day)
-            while (dateTemp <= datetime.today()):
-                conn.execute("INSERT INTO MessageCounts (ID, DATE) \
-                             VALUES (?, ?);", user.id, dateTemp)
-                dateTemp = dateTemp + datetime.timedelta(days=1)
 
         # index all messages
         for channel in guild.channels:  # for each channel
@@ -341,16 +415,28 @@ async def update(context):  # remakes the database of message counts
                         messageCountDatabase[message.author][sentTime] += 1
                         totalMessages += 1
                 except:
-                    print(
-                        "problem encountered for user: " + message.author.name + ". Message link: " + message.jump_url)
+                    print("problem encountered for user: " + message.author.name + ". Message link: " + message.jump_url)
                 if (totalMessages % 1000 == 0):
                     print(str(totalMessages) + " messages indexed.")
 
-        # update database
-        # for each user
-            # for each date
-                # update
-        conn.execute("UPDATE MessageCounts")
+        for user in guild.members:
+            # Get their earliest message date, or the date they joined, whichever is oldest.
+            dateTemp = datetime(user.joined_at.year, user.joined_at.month, user.joined_at.day)
+            for date in list(messageCountDatabase[user].keys()):
+                if (date < dateTemp):
+                    dateTemp = datetime(date.year, date.month, date.day)
+
+            # for each date since the user joined, fill in the database.
+            while (dateTemp <= datetime.today()):
+                if dateTemp in messageCountDatabase[user].keys():
+                    conn.execute("INSERT INTO MessageCounts (ID, DATE, MESSAGECOUNT) \
+                                VALUES (?, ?, ?);", (user.id, dateTemp, messageCountDatabase[user][dateTemp]))
+                else:
+                    conn.execute("INSERT INTO MessageCounts (ID, DATE) \
+                                VALUES (?, ?);", (user.id, dateTemp))
+                dateTemp = dateTemp + timedelta(days=1)
+
+        conn.commit()
 
         initializeStats(guild)
         endTime = time.time()
@@ -382,8 +468,6 @@ def initializeStats(guild):
     # general stats
     for user in guild.members:
         messageCountList = list(messageCountDatabase[user].values())
-        print("user: " + user.name);
-        print(messageCountList);
         userStatsMap[user] = {
             "total messages": sum(messageCountList),
             "mean": statistics.mean(messageCountList),
@@ -423,6 +507,18 @@ def getUser(context):
     else:
         return context.author
 
+def getUserGraphs(context):
+    content = context.message.content
+    mentions = []
+
+    if (content.find("all") != -1):
+        return context.message.guild.members
+    elif (len(context.message.mentions) != 0):
+        mentions = context.message.mentions
+    else:
+        mentions.append(context.author)
+
+    return mentions
 
 # Function obtained on stack overflow.
 # https://stackoverflow.com/questions/4563272/how-to-convert-a-utc-datetime-to-a-local-datetime-using-only-standard-library
@@ -434,6 +530,13 @@ def utc_to_local(utc_dt):
 # Intializes the aristocrats list with authorized users.
 async def initializeAristocrats():
     global aristocrats
-    aristocrats = [await bot.fetch_user(), await bot.fetch_user()]
+    aristocrats = [await bot.fetch_user(121007634714329089), await bot.fetch_user(237353382229049345)]
 
-bot.run('')
+
+async def getServer(context):
+    global conn, DBName
+
+    DBName = str(context.message.guild.id) + ".db"
+    conn = sqlite3.connect(DBName, detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES)
+
+bot.run('MTAxNTA3MjU0MTA0NTQ0MDUyMw.G_i4l4.v91kz0-7QX5NsGzs3EZGiuW3BD3z2Dp6C3QIEc')
